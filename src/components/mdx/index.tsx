91e7b8b3747800d3757b46d7977378c602ed07e8
Adrià Compte <muniatu@gmail.com>
2026-05-04 02:16:09 +0200

    wip: nespresso color filter case study draft + InterviewBubbles MDX component

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,8 +1,10 @@
 import type { MDXComponents } from "mdx/types";
 import ProjectImage from "./ProjectImage";
 import ImageRow from "./ImageRow";
+import InterviewBubbles from "./InterviewBubbles";
 
 export const mdxComponents: MDXComponents = {
   ProjectImage,
   ImageRow,
+  InterviewBubbles,
 };
